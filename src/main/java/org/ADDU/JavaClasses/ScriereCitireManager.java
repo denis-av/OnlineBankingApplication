4ba6b576e17049408cee864abf104ebe00d329ff
avram <avram.denis@domain.com>
2020-05-05 18:59:30 +0300

    [OBA-9] - As a client, I want to be able to request a new loan / Create a Java Class specific for managers to add a new request from each client

---
+++ src/main/java/org/ADDU/JavaClasses/ScriereCitireManager.java
@@ -0,0 +1,166 @@
+package org.ADDU.JavaClasses;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.ADDU.Model.Client;
+import org.ADDU.Model.Manager;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+public class ScriereCitireManager {
+    private int idManager;
+
+    public Manager returnManager(String MANAGERID){
+        Manager items = new Manager();
+        Manager manager = null;
+        Loan loan=new Loan();
+        try {
+            manager=new Manager();
+            File inputFile = new File("src\\main\\resources\\manager.xml");
+
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(inputFile);
+            doc.getDocumentElement().normalize();
+
+            Node Manager = doc.getFirstChild();
+            for(int j=0;j<doc.getElementsByTagName("Manager").getLength();j++) {
+                Node eachClient = doc.getElementsByTagName("Manager").item(j);
+
+                NamedNodeMap attr = eachClient.getAttributes();
+                Node nodeAttr = attr.getNamedItem("id");
+                manager.setId(Integer.parseInt(nodeAttr.getTextContent()));
+
+                NodeList list = eachClient.getChildNodes();
+
+                for (int i = 0; i < list.getLength(); i++) {
+
+                    Node node = list.item(i);
+
+                    if ("username".equals(node.getNodeName())) {
+                        manager.setUsername(node.getTextContent());
+                    }
+
+                    if ("password".equals(node.getNodeName())) {
+                        manager.setPassword(node.getTextContent());
+                    }
+                    if ("firstName".equals(node.getNodeName())) {
+                        manager.setFirstName(node.getTextContent());
+                    }
+                    if ("lastName".equals(node.getNodeName())) {
+                        manager.setLastName(node.getTextContent());
+                    }
+                    if ("cards".equals(node.getNodeName())) {
+                        NodeList cards = node.getChildNodes();
+                        for (int p = 0; p < cards.getLength(); p++) {
+                            Node test = cards.item(p);
+                            if ("cardNumber".equals(test.getNodeName())) {
+                                manager.addCard(test.getTextContent());
+                            }
+                        }
+                    }
+                    if ("loans".equals(node.getNodeName())) {
+                        NodeList loans = node.getChildNodes();
+                        for (int k = 0; k < loans.getLength(); k++) {
+                            NodeList loans2 = loans.item(k).getChildNodes();
+                            for (int p = 0; p < loans2.getLength(); p++) {
+                                Node test = loans2.item(p);
+
+                                if ("desiredAmount".equals(test.getNodeName())) {
+                                    loan.setDesiredAmount(test.getTextContent());
+                                }
+                                if ("status".equals(test.getNodeName())) {
+                                    loan.setStatus(test.getTextContent());
+                                }
+                                if ("message".equals(test.getNodeName())) {
+                                    loan.setMessage(test.getTextContent());
+                                }
+                                if ("usernameClient".equals(test.getNodeName())) {
+                                    loan.setUsernameClient(test.getTextContent());
+                                    manager.addLoan(loan);
+                                    loan = new Loan();
+                                }
+                            }
+                        }
+                    }
+                    if (manager.getId() == (Integer.parseInt(MANAGERID)-1)){
+                        items = manager;
+                        manager = new Manager();
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return items;
+    }
+
+    public void scriereManager(String amount, Client client, Manager managerClient){
+        try{
+            String filepath = "src\\main\\resources\\manager.xml";
+            DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+            Document doc = docBuilder.parse(filepath);
+
+            Node Client = doc.getFirstChild();
+
+
+            Node manager = doc.getElementsByTagName("Manager").item(managerClient.getId());
+            Node loans = doc.getElementsByTagName("loans").item(managerClient.getId());
+
+
+            NodeList list = manager.getChildNodes();
+
+            for (int i = 0; i < list.getLength(); i++) {
+
+                Node node = list.item(i);
+
+                if ("loans".equals(node.getNodeName())) {
+                    Element loan=doc.createElement("loan");
+                    Element desiredAmount=doc.createElement("desiredAmount");
+                    desiredAmount.appendChild(doc.createTextNode(amount));
+                    Element usernameClient=doc.createElement("usernameClient");
+                    usernameClient.appendChild(doc.createTextNode(client.getUsername()));
+                    loan.appendChild(desiredAmount);
+                    loan.appendChild(usernameClient);
+                    loans.appendChild(loan);
+                }
+
+
+            }
+
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            Transformer transformer = transformerFactory.newTransformer();
+            DOMSource source = new DOMSource(doc);
+            StreamResult result = new StreamResult(new File(filepath));
+            transformer.transform(source, result);
+
+            //System.out.println("Done");
+
+        }catch (ParserConfigurationException pce) {
+            pce.printStackTrace();
+        } catch (TransformerException tfe) {
+            tfe.printStackTrace();
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        } catch (SAXException sae) {
+            sae.printStackTrace();
+        }
+    }
+}
